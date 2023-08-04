c060d54566d00831b321f47367fabe0fdc3da38c
mthhenri <mathsilvaesilva@gmail.com>
2023-08-04 11:09:39 -0300

    Dia 01 Final

---
+++ dia.01/ProjetoInicial/Program.cs
@@ -0,0 +1,57 @@
+﻿int resp;
+do(
+    Console.WriteLine("Ir para onde?");
+    resp = Convert.ToInt32(Console.ReadLine());
+    switch(resp){
+    case 1:
+        Ex01();
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+    case 5:
+        break;
+    case 6:
+        break;
+    default:
+        break;
+    }
+)while(resp != 7);
+
+static void Ex01(){
+    string sair = "";
+    do{
+        int largura;
+        Console.WriteLine("Informe a Largura: ");
+        largura = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Informe a Altura: ");
+        int altura = Convert.ToInt32(Console.ReadLine());
+        int area = largura * altura;
+        Console.WriteLine("A área é: " + area);
+        Console.WriteLine("Deseja sair? [S/N]");
+        sair = Console.ReadLine();
+    }while(sair.Equals("S") || sair.Equals("s"));
+}
+
+void Ex02(){
+    
+}
+
+void Ex03(){
+    
+}
+
+void Ex04(){
+    
+}
+
+void Ex05(){
+    
+}
+
+void Ex06(){
+    
+}

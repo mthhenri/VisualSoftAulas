6987c03f44ff87cf654ff0f14e843dc04e9a1a58
Matheus <mathsilvaesilva@gmail.com>
2023-08-07 17:57:37 -0300

    Atividade 2

--- dia.01/ProjetoInicial/Program.cs
+++ dia.01/ProjetoInicial/Program.cs
@@ -7,14 +7,19 @@ do{
         Ex01();
         break;
     case 2:
+        Ex02();
         break;
     case 3:
+        Ex03();
         break;
     case 4:
+        Ex04();
         break;
     case 5:
+        Ex05();
         break;
     case 6:
+        Ex06();
         break;
     default:
         break;
@@ -31,13 +36,28 @@ static void Ex01(){
         int altura = Convert.ToInt32(Console.ReadLine());
         int area = largura * altura;
         Console.WriteLine("A área é: " + area);
+        Console.WriteLine()
         Console.WriteLine("Deseja sair? [S/N]");
         sair = Console.ReadLine();
     }while(sair.Equals("S") || sair.Equals("s"));
 }
 
 void Ex02(){
-    
+    string sair = "";
+    do{
+        Console.WriteLine("Informe o valor para ser concertido (em R$)");
+        int real = Convert.ToInt32(Console.ReadLine());
+        int dolar = real / 5.18;
+        int euro = real / 5.31;
+        int peso = real / 0.04;
+        Console.WriteLine(real + " reais são equivalentes a:")
+        Console.WriteLine(dolar + " dólares")
+        Console.WriteLine(euro + " euros")
+        Console.WriteLine(peso + " pesos argentinos")
+        Console.WriteLine()
+        Console.WriteLine("Deseja sair? [S/N]");
+        sair = Console.ReadLine();
+    }while(sair.Equals("S") || sair.Equals("s"))
 }
 
 void Ex03(){
